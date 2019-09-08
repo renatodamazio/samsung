7baf6d61320f0fdfbd0c0dd990a1485abbf1ece0
Renato <renatopdamazio@gmail.com>
2019-09-08 12:13:23 -0300

    Done - Apply all features.

--- app/public/static/js/form.js
+++ app/public/static/js/form.js
@@ -1,22 +1,65 @@
 $(document).ready(function() {
-		// validate the comment form when it is submitted
-		$("form").validate({
-      messages: {
-        nome: "Preencha seu nome",
-        sobrenome:  "Preencha seu sobre-nome",
-        email:  "Preencha seu e-mail",
-        telefone: "Preencha seu telefone",
-        
-      }
-    });
+			$(function() {
 
-    $("form input").focus(function() {
+				jQuery.validator.addMethod("cellphone", function (value, element) {
+						value = value.replace("(", "");
+						value = value.replace(")", "");
+						value = value.replace("-", "");
+						value = value.replace(" ", "");
 
-      $(this).prev('.label-input').addClass('active-label-input')
+						console.log(this.optional(element) || /[0-9]{11}/.test(value))
+						return this.optional(element) || /[0-9]{11}/.test(value);
+				}, "Por favor, um telefone válido");
 
-    }).blur(function() {
 
-      $(this).prev('.label-input').removeClass('active-label-input')
+				$("#contact-form").validate({
+					rules: {
+						telefone: {
+							cellphone: true,
+							required: true
+						},
+				    field: {
+				      required: true,
+				      email: true
+				    }
+				  },
+			    messages: {
+			      nome: "Preencha seu nome",
+			      sobrenome:  "Preencha seu sobre-nome",
+			      email:  "Preencha seu e-mail",
+			      telefone: "Preencha seu telefone",
+						accpetTerms: "É preciso aceitar os termos e condições da Sansumg"
+			    }
+			  });
 
-    })
+			});
+
+	    $("form input").focus(function() {
+
+	      $(this).prev('.label-input').addClass('active-label-input')
+
+	    }).blur(function() {
+	      if ($(this).val().length == 0) {
+
+	        $(this).prev('.label-input').removeClass('active-label-input')
+	      }
+	    })
+
+			var maskBehavior = function(val) {
+
+	      return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+
+	    }, options = {
+
+	        onKeyPress: function(val, e, field, options) {
+
+	          field.mask(maskBehavior.apply({}, arguments), options);
+
+	        }
+
+	    };
+
+	    $('#telefone').mask(maskBehavior, options);
+
+
 })
